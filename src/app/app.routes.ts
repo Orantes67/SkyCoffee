970e517607c4c4fa8f5c82761fe340dac347725e
Hector <orantesroman39@gmail.com>
2025-11-16 19:30:21 -0600

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {  Routes } from '@angular/router';
+import { LandingComponent } from './views/landing/landing.component';
+import { LoginComponent } from './views/login/login.component';
+import { SingupComponent } from './views/singup/singup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+  { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'singup', component: SingupComponent }
+];
---
+++ src/app/core/models/ usuario.model.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+  id?: number;
+  nombre: string;
+  correo: string;
+  telefono: string;
+  foto_perfil: string;
+}
